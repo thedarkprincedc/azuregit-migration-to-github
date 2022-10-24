aba34768f1e53c62e93aae1174eefabb689334e2
Brett <brettmosley@hotmail.com>
2022-10-24 09:37:46 -0400

    Update get-azure-devops-repos-onpremise.ps1

--- get-azure-devops-repos-onpremise.ps1
+++ get-azure-devops-repos-onpremise.ps1
@@ -1,1 +1,15 @@
+param(
+    [string]$pat = $env:AZ_DEVOPS_PAT,
+    [string]$organization = $env:AZ_DEVOPS_ORGANIZATION,
+    [string]$format = "csv"
+)
 
+. ./common/common.ps1
+
+$authToken = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)"))
+$request = getAzureRepositoriesOnPremise -organization $organization -authToken $authToken
+
+switch($format){
+    "csv" { format-elements-as-csv -objects $request; break; }
+    "list" { format-elements-as-list -objects $request; break; }
+}
